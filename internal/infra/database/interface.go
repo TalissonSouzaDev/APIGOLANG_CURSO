b40724b6a4437d1ccc3dd70da8fe78b9f3deee5b
TalissonSouza <talissonmdsouza@gmail.com>
2024-06-26 15:44:05 -0300

    Crud de Produtos com paginação

--- internal/infra/database/interface.go
+++ internal/infra/database/interface.go
@@ -6,3 +6,11 @@ type UserInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(emaild string) (*entity.User, error)
 }
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindById(id string)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
